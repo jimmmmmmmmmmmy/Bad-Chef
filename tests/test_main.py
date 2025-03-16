4e5a450a9e4b51984abc896d7fe428f039f0b695
James Liu <jamesliu@Jamess-Mac-mini.local>
2025-03-16 15:35:39 -0400

    Added tests for user login
    - test_good_login_user() for valid user credentials
    - test_bad_login_pw() for valid username, invalid password
    - test_bad_login_user() for invalid username, someones password

--- tests/test_main.py
+++ tests/test_main.py
@@ -23,3 +23,59 @@ def test_create_user(client, test_db):
     assert "password" not in data  # Password shouldn’t be returned
     db_user = test_db.exec(select(User).where(User.username == "testuser")).first()
     assert pwd_context.verify("password123", db_user.hashed_password)
+
+def test_good_login_user(client, test_db):
+    user_data = {
+        "username": "testuser",
+        "email": "test@example.com",
+        "password": "password123"
+    }
+    client.post("/users/", json=user_data)
+
+    # Test successful login
+    login_data = {
+        "username": "testuser",
+        "password": "password123"
+    }
+    response = client.post("/token", json=login_data)
+    print(response.text)
+    assert response.status_code == 200
+    data = response.json()
+    assert "access_token" in data
+    assert data["token_type"] == "bearer"
+
+def test_bad_login_pw(client, test_db):
+    user_data = {
+        "username": "testuser",
+        "email": "test@example.com",
+        "password": "password123"
+    }
+    client.post("/users/", json=user_data)
+
+    # Test successful login
+    login_data = {
+        "username": "testuser",
+        "password": "password"
+    }
+    response = client.post("/token", json=login_data)
+    print(response.text)
+    assert response.status_code == 401
+    assert response.json()["detail"] == "Invalid credentials"
+
+def test_bad_login_user(client, test_db):
+    user_data = {
+        "username": "testuser",
+        "email": "test@example.com",
+        "password": "password123"
+    }
+    client.post("/users/", json=user_data)
+
+    # Test successful login
+    login_data = {
+        "username": "testuser123",
+        "password": "password123"
+    }
+    response = client.post("/token", json=login_data)
+    print(response.text)
+    assert response.status_code == 401
+    assert response.json()["detail"] == "Invalid credentials"
